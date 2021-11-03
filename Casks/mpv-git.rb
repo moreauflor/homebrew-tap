6923ebbf416db774dca61a52ee06df73053452b9
NextFire <git@yuru.moe>
2021-11-03 17:19:18 +0100

    mpv: zip -> tar

--- Casks/mpv-git.rb
+++ Casks/mpv-git.rb
@@ -2,7 +2,7 @@ cask "mpv-git" do
   version "0.33.0-388-g254730d891"
   sha256 "918aea4e70042bf3a7af44c0acee050b7000ee0b694163d8d93d2ec44da62e76"
 
-  url "https://github.com/NextFire/mpv-macos-git-builds/releases/download/mpv-#{version}/mpv-#{version}.zip",
+  url "https://github.com/NextFire/mpv-macos-git-builds/releases/download/mpv-#{version}/mpv-#{version}.tar",
       verified: "github.com/NextFire/mpv-macos-git-builds/"
   name "mpv"
   desc "Media player based on MPlayer and mplayer2"
