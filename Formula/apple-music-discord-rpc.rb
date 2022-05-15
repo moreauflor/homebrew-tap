74f2e5a817ec96783d4ea35ac356f705cfd4d279
NextFire <git@yuru.moe>
2022-05-15 13:14:15 +0200

    apple-music-discord-rpc formula (closes NextFire/apple-music-discord-rpc#21)

---
+++ Formula/apple-music-discord-rpc.rb
@@ -0,0 +1,22 @@
+class AppleMusicDiscordRpc < Formula
+  desc "Discord Rich Presence client for the macOS Apple Music app"
+  homepage "https://github.com/NextFire/apple-music-discord-rpc"
+  url "https://github.com/NextFire/apple-music-discord-rpc/archive/refs/tags/0.2.0.tar.gz"
+  sha256 "9e0a5981c33c2b06a3b36ebf0b5b8ab7424f058aee348846328d2f4349a7bc9b"
+  head "https://github.com/NextFire/apple-music-discord-rpc.git", branch: "main"
+  license "MIT"
+
+  depends_on "deno"
+  
+  def install
+    bin.install "music-rpc.ts"
+  end
+
+  service do
+    run bin/"music-rpc.ts"
+    keep_alive true
+    environment_variables PATH: std_service_path_env
+    working_dir var/"apple-music-discord-rpc"
+    error_log_path "music-rpc.log"
+  end
+end
