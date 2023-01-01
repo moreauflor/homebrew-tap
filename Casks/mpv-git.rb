12a2d5c5ebac61da979e75a291555b1c0f67987a
Nam Vu <git@yuru.moe>
2023-01-01 20:39:35 +0100

    Update mpv-git.rb

--- Casks/mpv-git.rb
+++ Casks/mpv-git.rb
@@ -1,6 +1,6 @@
 cask "mpv-git" do
   version "0.35.0-67-gad65c8855b"
-  sha256 ""
+  sha256 "cdf8f80d0b912a9f59220639137745dec40b961e0c37d4ef965018be19676838"
 
   url "https://github.com/NextFire/mpv-macos-git-builds/releases/download/mpv-#{version}/mpv-#{version}.tar",
       verified: "github.com/NextFire/mpv-macos-git-builds/"
