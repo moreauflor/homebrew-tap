dc940692d375db32035b1f35dd4f0f28b74b93c1
Nam Vu <git@yuru.moe>
2023-09-14 11:38:08 -0400

    Update syncplay-japan7.rb

--- Casks/syncplay-japan7.rb
+++ Casks/syncplay-japan7.rb
@@ -1,8 +1,8 @@
 cask "syncplay-japan7" do
-  version "b80dbb06"
+  version "eaec6b15"
   sha256 :no_check
 
-  url "https://hikari.butaishoujo.moe/b/#{version}/Syncplay_1.7.0.dmg",
+  url "https://hikari.butaishoujo.moe/b/#{version}/Syncplay_1.7.1.dmg",
       verified: "hikari.butaishoujo.moe/"
   name "Syncplay-Japan7"
   desc "Synchronises media players"
