3837f1f88cd5852a86a50d61f07714c54fc3e681
moreauflor <155857849+moreauflor@users.noreply.github.com>
2024-01-06 20:54:21 -0500

    Update apple-music-discord-rpc.rb

--- Formula/apple-music-discord-rpc.rb
+++ Formula/apple-music-discord-rpc.rb
@@ -2,7 +2,7 @@ class AppleMusicDiscordRpc < Formula
   desc "Discord Rich Presence client for the macOS Apple Music app"
   homepage "https://github.com/moreauflor/apple-music-discord-rpc"
   url "https://github.com/moreauflor/apple-music-discord-rpc/releases/tag/0.8.1"
-  sha256 "89d0b5d86082b170b46d7d4bffbbb829c4c9431b9ea77697a6014f8fba687b85"
+  sha256 "68e465fa5253be60b4c38a80c375c041902536b50d92ad2b1d694330c1b8ee19"
   license "MIT"
   head "https://github.com/moreauflor/apple-music-discord-rpc.git", branch: "main"
 
